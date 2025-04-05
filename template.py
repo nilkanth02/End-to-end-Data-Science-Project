84e3263f36ba119420a40da794c2d8908fe118fc
unknown <nilkanth8747@gmail.com>
2025-04-05 13:50:18 +0530

    Created a File structure Using Template.py

---
+++ template.py
@@ -0,0 +1,45 @@
+# This is a Template File for creating the folder and files for any project that is having the same structure.
+
+
+import os 
+from pathlib import Path 
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+project_name = "mlproject"
+
+list_of_files = [
+    # ".github/workflows/.gitkeep" ,# this tells that we are using or creating the git folder for the project 
+    f"src/{project_name}/__init__.py",   # this is uses when we want to make it as a package 
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_ingestion.py",
+    f"src/{project_name}/components/data_transformation.py",
+    f"src/{project_name}/components/data_trainer.py",
+    f"src/{project_name}/components/data_monitoring.py",
+    f"src/{project_name}/pipelines/__init__.py",
+    f"src/{project_name}/pipelines/training_pipeline.py",
+    f"src/{project_name}/pipelines/prediction_pipeline.py",
+    f"src/{project_name}/exception.py",
+    f"src/{project_name}/utils.py",
+    "app.py",
+    "Dockerfile",
+    "setup.py"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}  ")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
